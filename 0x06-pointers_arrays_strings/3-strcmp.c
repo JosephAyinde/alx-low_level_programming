087628ada74203fa121042c4a9e6c1d2c8fde96a
Joseph Ayinde <josephayinde36@gmail.com>
2022-09-21 15:02:52 -0700

    hot fix task 3

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -1,12 +1,12 @@
 #include "main.h"
 
 /**
-* _strcmp - function which compare two strings and 
+*  _strcmp - function which compare two strings and 
 *@s1: first string
 *@s2:second string
 *Return:
 *		returns zero if s1 == s2
-		returns negative number if s1 < s2
+*		returns negative number if s1 < s2
 *		returns positive number if s1 > s2
 */
 
