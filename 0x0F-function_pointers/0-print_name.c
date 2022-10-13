4d6260e46867959085a8243ed6fbf3bfaf0528c8
Joseph Ayinde <josephayinde36@gmail.com>
2022-10-13 06:07:34 -0700

    TASK 0

--- 0x0F-function_pointers/0-print_name.c
+++ 0x0F-function_pointers/0-print_name.c
@@ -1,36 +1,18 @@
-nclude "function_pointers.h"
+#include "main.h"
+#include <stdlib.h>
 
 /**
- *  * print_name - prints a name.
- *   * @name: input name.
- *    * @f: function pointer.
- *     *
- *      * Return: no return.
- *       */
-void print_name(char *name, void (*f)(char *))
+ * malloc_checked - allocates memory using malloc
+ * @b: memory to allocate
+ *
+ * Return: pointer to allocated memory or normal process termination if error
+ */
+void *malloc_checked(unsigned int b)
 {
-		if (name && f)
-					f(name);
+	void *p;
+
+	p = malloc(b);
+	if (p == NULL)
+		exit(98);
+	return (p);
 }
-
-1-array_iterator.c
-#include "function_pointers.h"
-
-/**
- *  * array_iterator - executes a function given as a
- *   * parameter on each element of an array.
- *    * @array: input integer array.
- *     * @size: size of the array.
- *      * @action: pointer to the function.
- *       *
- *        * Return: no return.
- *         */
-void array_iterator(int *array, size_t size, void (*action)(int))
-{
-		unsigned int i;
-
-			if (array && action)
-						for (i = 0; i < size; i++)
-							 			action(array[i]);
-}
-
